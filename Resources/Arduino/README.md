a789ca4421c2fe13e1ebc87552d50804eaf58741
Robert Powers <rdpowers@gmail.com>
2013-12-11 19:19:15 +1100

    Initial Firmata support and pins_arduino fixes.

--- Resources/Arduino/README.md
+++ Resources/Arduino/README.md
@@ -1,7 +1,17 @@
 This is still in progress.
 
+Arduino Essentials
+------------
 You'll need the Zigduino core folder copied to <arduinoroot>/hardware/arduino/cores. - (ex. ~/arduino-1.0.5/hardware/arduino/cores/zigduino)
 
 pins_arduino.h needs to go in a folder under <arduino root>/hardware/arduino/variants/meshthing - (ex. ~/arduino-1.0.5/hardware/arduino/variants/meshthing/pins_arduino.h)
 
 boards.txt needs to replace the file in <arduino root>/hardware/arduino - (ex. ~/arduino-1.0.5/hardware/arduino/boards.txt)
+
+Arduino Libraries
+-------------
+The libraries subfolder has modified Arduino standard libraries for use with the Meshthing.
+Currently only Firmata has been modified though other libraries may work out of the box (Ex. SDCard). To use firmata:
+Boards.h needs to replace existing file in <arduino root>/libraries/Firmata/Boards.h - (ex. ~/arduino-1.0.5/libraries/Firmata/Boards.h)
+
+!!Be careful when using with a MeshUSB as the MeshUSB utilizes AD2/AD3 for battery monitoring and should be set as inputs!! Firmata seems to want to set these as outputs by default. It doesn't seem to cause any trouble, but may result in unexpected behaviour or damage.
--- Resources/Arduino/meshthing/pins_arduino.h
+++ Resources/Arduino/meshthing/pins_arduino.h
@@ -27,7 +27,7 @@
 	Pierce Nichols 2011 Oct 11
 
 	Modified for the meshthing
-	Robert Powers 2013 Sep
+	Robert Powers 2013 Sep/Dec
 */
 
 #ifndef Pins_Arduino_h
@@ -35,12 +35,23 @@
 
 #include <avr/pgmspace.h>
 
-#define NUM_DIGITAL_PINS            15
-#define NUM_ANALOG_INPUTS           7
-#define analogInputToDigitalPin(p)  ((p < 6) ? (p) + 14 : -1)
-#define digitalPinHasPWM(p)         ((p) == 3 ||(p) == 4 ||(p) == 6 ||(p) == 8 ||(p) == 9 ||(p) == 10 ||(p) == 11)
+//This is the number of I/O pins, including those that double as analogue.
+#define NUM_DIGITAL_PINS            28
 
+#define NUM_ANALOG_INPUTS           7
+
+//This is some strange thing to do with addressing analogue pins as digital(?). 
+//It seems like others have been putting this offset as A0->MAX(DigitalPin#)+1
+//so for Meshthing if you get D21 -> A0
+#define analogInputToDigitalPin(p)  ((p < 7) ? (p) + 21 : -1)
+
+
+#define digitalPinHasPWM(p)         ( (p) == 4 || (p) == 10 || (p) == 14 || (p) == 15 )
+
+//Addressing here seems to be at the chip pinout level.(?)-RDP
 /*const static uint8_t SS   = 10*/
+/*
+const static uint8_t SS   = 26;
 const static uint8_t MOSI = 28;
 const static uint8_t MISO = 29;
 const static uint8_t SCK  = 27;
@@ -56,31 +67,57 @@ const static uint8_t A3 = 1;
 const static uint8_t A4 = 2;
 const static uint8_t A5 = 3;
 const static uint8_t A6 = 4;
+*/
 
+//Using arduino digital pin mappings to specifc keywords
+const static uint8_t SS   = 17;
+const static uint8_t MOSI = 11;
+const static uint8_t MISO = 12;
+const static uint8_t SCK  = 13;
+
+const static uint8_t SDA = 18;
+const static uint8_t SCL = 16;
+const static uint8_t LED = 13;
+
+const static uint8_t A0 = 21;
+const static uint8_t A1 = 22;
+const static uint8_t A2 = 23;
+const static uint8_t A3 = 24;
+const static uint8_t A4 = 25;
+const static uint8_t A5 = 26;
+const static uint8_t A6 = 27;
+
+
+// Disregard this comment - RDP
 // A majority of the pins are NOT PCINTs, SO BE WARNED (i.e. you cannot use them as receive pins)
 // Only pins available for RECEIVE (TRANSMIT can be on any pin):
 // Pins: 4,8,10,11,12
 
-#define digitalPinToPCICR(p)    ( ( (((p) >= 10) && ((p) <= 12)) || ((p) == 4) || ((p) == 8)) ) ? (&PCICR) : ((uint8_t *)0) )
+//I'm not sure which addressing is used for all of this. I'm using the chip pinout. Could be this is supposed to use the arduino pinout?
+//Still checking... -RDP
+//#define digitalPinToPCICR(p)    ( ( (((p) >= 26) && ((p) <= 34)) ) ? (&PCICR) : ((uint8_t *)0) )
+//Using Arduino digital pin mapping to set which pins have pin change interrupts.
+#define digitalPinToPCICR(p)    ( ( (((p) >= 10) && ((p) <= 14)) || ((p) == 17) || ((p) == 19) ) ? (&PCICR) : ((uint8_t *)0) )
 
-//I don't know what this does - RDP
+//I don't know what this does 
+//Looks like something to do with a PC control register, but I can't suss it out. - RDP
 #define digitalPinToPCICRbit(p) ( ((p) == 7) ? 1 : 0 ) 
 
-//This either - RDP
-#define digitalPinToPCMSK(p)    ( ((((p) >= 8) && ((p) <= 13)) || ((p) == 20)) ? (&PCMSK0) : \
-                                ( ((p) == 7) ? (&PCMSK1) : \
+//This returns the PC mask register associated with each pin. PCINT0-7 are on PCMSK0, PCINT8 is on PCMSK1
+#define digitalPinToPCMSK(p)    ( ( (((p) >= 10) && ((p) <= 14)) || ((p) <= 17) ) ? (&PCMSK0) : \
+                                ( ((p) == 19) ? (&PCMSK1) : \
                                 ((uint8_t *)0) ) )
 
-//This. Complete mystery right now.
-#define digitalPinToPCMSKbit(p) ( ((p) == 7) ? 0 : \
-                                ( ((p) == 8) ? 4 : \
-                                ( ((p) == 9) ? 7 : \
-                                ( ((p) == 10) ? 6 : \
-                                ( ((p) == 11) ? 5 : \
-                                ( ((p) == 12) ? 3 : \
+//Looks like this maps the pin to the PC mask register control bit
+// PCINT0-7 are 0-7 on PCMSK0, PCINT8 is bit 0 on PCMSK1
+#define digitalPinToPCMSKbit(p) ( ((p) == 17) ? 0 : \
                                 ( ((p) == 13) ? 1 : \
-                                ( ((p) == 20) ? 2 : \
-                                0 ) ) ) ) ) )
+                                ( ((p) == 11) ? 2 : \
+                                ( ((p) == 12) ? 3 : \
+                                ( ((p) == 14) ? 4 : \
+                                ( ((p) == 10) ? 5 : \
+                                ( ((p) == 19) ? 0 : \
+                                0 ) ) ) ) ) ) )
 
 #ifdef ARDUINO_MAIN
 
@@ -116,11 +153,13 @@ const uint16_t PROGMEM port_to_output_PGM[] = {
 	NOT_A_PORT,
 };
 
+//I don't know what this does... might need a look. 
+//It looks like it's saying where input ports exist... -RDP
 const uint16_t PROGMEM port_to_input_PGM[] = {
 	NOT_A_PIN,
 	NOT_A_PIN,
+	(uint16_t)&PINB,
 	NOT_A_PIN,
-	(uint16_t)&PINC,
 	(uint16_t)&PIND,
 	(uint16_t)&PINE,
 	(uint16_t)&PINF,
@@ -137,72 +176,66 @@ const uint8_t PROGMEM digital_pin_to_port_PGM[] = {
 	// -------------------------------------------	
 
 	//THIS PINOUT IS USED FOR SDCARD ACCESSS, NO LONGER BREAKS BLINK EXAMPLE! (as of v1.1)
-	PF	, // PF 0 ** 0 ** DIO0/AD0	-- 20
-	PF	, // PF 1 ** 1 ** DIO1/AD1	-- 19
-	PF	, // PF 2 ** 2 ** DIO2/AD2	-- 18
-	PF	, // PF 3 ** 3 ** DIO3/AD3  	-- 17
-	PE	, // PE 4 ** 4 ** DIO4		-- 11
-	PF	, // PF 5 ** 5 ** DIO5/AD5/Assoc-- 15
-	PF	, // PF 6 ** 6 ** DIO6/AD6/~RTS	-- 16
-	PF	, // PF 7 ** 7 ** DIO7/AD7/~CTS -- 12
-	PD	, // PD 3 ** 8 ** DO8/UART1 TX/INT3-- 4
-	PD	, // PD 1 ** 9 ** SDA/~DTR/DI8  -- 9
-	PB	, // PB 5 ** 10 ** PWM0/RSSI	-- 6
+	PF	, // PF 0 ** 0 ** DIO0/AD0	-- XBee Pin 20
+	PF	, // PF 1 ** 1 ** DIO1/AD1	-- XBee Pin 19
+	PF	, // PF 2 ** 2 ** DIO2/AD2	-- XBee Pin 18
+	PF	, // PF 3 ** 3 ** DIO3/AD3  	-- XBee Pin 17
+	PE	, // PE 4 ** 4 ** DIO4		-- XBee Pin 11
+	PF	, // PF 5 ** 5 ** DIO5/AD5/Assoc-- XBee Pin 15
+	PF	, // PF 6 ** 6 ** DIO6/AD6/~RTS	-- XBee Pin 16
+	PF	, // PF 7 ** 7 ** DIO7/AD7/~CTS -- XBee Pin 12
+	PD	, // PD 3 ** 8 ** DO8/UART1 TX/INT3-- XBee Pin 4
+	PD	, // PD 1 ** 9 ** SDA/~DTR/DI8  -- I2C_SCA XBee Pin 9
+	PB	, // PB 5 ** 10 ** PWM0/RSSI	-- XBee Pin 6
 
 	PB	, // PB 2 ** 11 ** MOSI		-- SDCard Pin 3 (CMD/DI)
 	PB	, // PB 3 ** 12 ** MISO		-- SDCard Pin 7 (Dat0)
 	PB	, // PB 1 ** 13 ** SCK		-- SDCard Pin 5 (Clk)	
 
-	PB	, // PB 4 ** 14 ** PWM1		-- 7
-	PE	, // PE 5 ** 15 ** PWM3?(OC3C/Int5)--14	
-	PD	, // PD 0 ** 16 ** SCL/ON	-- 13
-	PE	, // PE 2 ** 17 ** SDCD/DAT3	-- SDCard Pin 2	
+	PB	, // PB 4 ** 14 ** PWM1		-- XBee Pin 7
+	PE	, // PE 5 ** 15 ** PWM3?(OC3C/Int5)-- XBee Pin 14	
+	PD	, // PD 0 ** 16 ** SCL/ON	-- I2C_SCL XBee Pin 13
+	PB	, // PB 0 ** 17 ** 		-- SDCard Pin 2 (CS/CD/DAT3)
 
 
 
 	PE	, // PE 1 ** 18 ** USART0_TX	-- XBee Pin 2
 	PE	, // PE 0 ** 19 ** USART0_RX	-- XBee Pin 3
 	PD	, // PD 2 ** 20 ** USART1_RX    -- XBee Pin 8
-	PD	, // PD 0 ** 21 ** 
-	PB	, // PB 0 ** 22 ** --Dummy PB0 for SD card?
-	PD  , // PD 5 ** 23 ** RFTX		--These aren't wired to anything either.
-	PD  , // PD 6 ** 24 ** RFRX
+
 };
 
 const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[] = {
 	// PIN IN PORT		
 	// -------------------------------------------
 	// Comments not up to date!		
-	_BV( 0 )	, // PE 6 ** 0 ** D2
-	_BV( 1 )	, // PE 5 ** 1 ** PWM3
-	_BV( 2 )	, // PE 2 ** 2 ** PWM4
-	_BV( 3 )	, // PE 3 ** 3 ** D5
-	_BV( 4 )	, // PE 4 ** 4 ** PWM6
-	_BV( 5 )	, // PE 7 ** 5 ** D7
-	_BV( 6 )	, // PB 4 ** 6 ** PWM8
-	_BV( 7 )	, // PB 7 ** 7 ** PWM9
-	_BV( 3 )	, // PB 6 ** 8 ** PWM10
-	_BV( 1 )	, // PB 5 ** 9 ** PWM11
-	_BV( 5 )	, // PB 5 ** 10 ** PWM11
+	_BV( 0 )	, // PF 0 ** 0 ** DIO0/AD0	-- 20
+	_BV( 1 )	, // PF 1 ** 1 ** DIO1/AD1	-- 19
+	_BV( 2 )	, // PF 2 ** 2 ** DIO2/AD2	-- 18
+	_BV( 3 )	, // PF 3 ** 3 ** DIO3/AD3  	-- 17
+	_BV( 4 )	, // PE 4 ** 4 ** DIO4		-- 11
+	_BV( 5 )	, // PF 5 ** 5 ** DIO5/AD5/Assoc-- 15
+	_BV( 6 )	, // PF 6 ** 6 ** DIO6/AD6/~RTS	-- 16
+	_BV( 7 )	, // PF 7 ** 7 ** DIO7/AD7/~CTS -- 12
+	_BV( 3 )	, // PD 3 ** 8 ** DO8/UART1 TX/INT3-- 4
+	_BV( 1 )	, // PD 1 ** 9 ** SDA/~DTR/DI8  -- 9
+	_BV( 5 )	, // PB 5 ** 10 ** PWM0/RSSI	-- 6
 
-	_BV( 2 )	, // PF 4 ** 11 ** A4
-	_BV( 3 )	, // PF 2 ** 12 ** A2	
-	_BV( 1 )	, // PF 3 ** 13 ** A3	
+	_BV( 2 )	, // PB 2 ** 11 ** MOSI		-- SDCard Pin 3 (CMD/DI)
+	_BV( 3 )	, // PB 3 ** 12 ** MISO		-- SDCard Pin 7 (Dat0)	
+	_BV( 1 )	, // PB 1 ** 13 ** SCK		-- SDCard Pin 5 (Clk)	
 
-	_BV( 4 )	, // PB 3 ** 14 ** SPI_MISO
-	_BV( 5 )	, // PD 0 ** 15 ** D0
-	_BV( 0 )	, // PF 0 ** 16 ** A0	
-	_BV( 2 )	, // PF 1 ** 17 ** A1	
+	_BV( 4 )	, // PB 4 ** 14 ** PWM1		-- 7
+	_BV( 5 )	, // PE 5 ** 15 ** PWM3?(OC3C/Int5)--14
+	_BV( 0 )	, // PD 0 ** 16 ** SCL/ON	-- 13	
+	_BV( 0 )	, // PB 0 ** 17 ** 		--SDCard Pin 2 (CS/CD/DAT3)	
 
 
 	
-	_BV( 1 )	, // PF 5 ** 18 ** A5
-	_BV( 0 )	, // PB 2 ** 19 ** SPI_MOSI
-	_BV( 2 )	, // PD 0 ** 20 ** I2C_SCL
-	_BV( 0 )	, // PD 1 ** 21 ** I2C_SDA	
-	_BV( 0 )	, // PD 1 ** 22 ** I2C_SDA	
-	_BV( 5 )  	, // PD 5 ** 23 ** RFTX
-	_BV( 6 )  	, // PD 6 ** 24 ** RFRX
+	_BV( 1 )	, // PE 1 ** 18 ** USART0_TX	-- XBee Pin 2
+	_BV( 0 )	, // PE 0 ** 19 ** USART0_RX	-- XBee Pin 3
+	_BV( 2 )	, // PD 2 ** 20 ** USART1_RX    -- XBee Pin 8
+
 };
 
 const uint8_t PROGMEM digital_pin_to_timer_PGM[] = {
@@ -230,10 +263,7 @@ const uint8_t PROGMEM digital_pin_to_timer_PGM[] = {
 	NOT_ON_TIMER	, // PF 4 ** 18 ** A4	
 	NOT_ON_TIMER	, // PF 5 ** 19 ** A5
 	NOT_ON_TIMER	, // PB 2 ** 20 ** SPI_MOSI
-	NOT_ON_TIMER	, // PD 0 ** 21 ** I2C_SCL
-	NOT_ON_TIMER	, // PD 1 ** 22 ** I2C_SDA	
-	NOT_ON_TIMER  	, // PD 5 ** 23 ** RFTX
-	NOT_ON_TIMER  	, // PD 6 ** 24 ** RFRX
+
 };	
 
 #endif
